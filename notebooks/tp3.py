660038587452159e1b15dd312c69de7f83e61653
Chloe Aujoulat <chloe.aujoulat19@gmail.com>
2023-11-12 16:48:46 +0100

    add new version of the script for tp3; q6 to do

--- notebooks/tp3.py
+++ notebooks/tp3.py
@@ -9,4 +9,57 @@ from werkzeug.utils import send_file
 from flask import send_file
 from flask import request
 
+###################
+### Q1 and 2
+###################
 
+app=Flask(__name__)
+
+@app.route("/td1/<filename>")
+@app.route("/td1/")
+def content_file(filename="index.html"):
+    with open(f"fichiers/td1/{filename}", "r") as filename:
+        f = filename.read()
+    try:
+        return send_file(f"fichiers/td1/{filename}", attachment_filename=filename)
+    except Exception as e:
+        return str(e)
+
+###################
+### Q3
+###################
+
+@app.route("/formproc", methods=["POST"])
+def print_message():
+    return("Vos données ont bien été prises en compte")
+
+###################
+### Q4, 5 and 7
+###################
+
+@app.route("/formproc", methods=["POST"])
+def print_message():
+    data = request.from
+    conn = sqlite3.connect("db.sqlite3")
+    try:
+        c = conn.cursor()
+        c.execute(f"INSERT INTO FormData VALUES ('{data['prenom']}', '{data['nom']}', '{data['genre']}')")
+        conn.commit()
+        conn.close()
+    return (f"""
+    <table border="black">
+        <tr>
+            <td>Nom</td>
+            <td>{data["nom"]}</td>
+        </tr>
+        <tr>
+            <td>Prénom</td>
+            <td>{data["prenom"]}</td>
+        </tr>
+        <tr>
+            <td>Genre</td>
+            <td>{data["genre"]}</td>
+        </tr>
+    </table>
+    <a href="{url_for('personnesBDD')}">Personnes inscrites</a>
+    """)
