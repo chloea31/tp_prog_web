cb3cc2acb211132b45e78aae1c82ce1d22071f79
Chloe Aujoulat <chloe.aujoulat19@gmail.com>
2023-11-12 16:15:30 +0100

    add new script for tp3 1st version

---
+++ notebooks/tp3.py
@@ -0,0 +1,12 @@
+#/usr/bin/python3
+#-*-coding: utf-8-*-
+
+import sqlite3
+from flask import Flask
+from flask.helpers import url_for
+from flask.scaffold import F
+from werkzeug.utils import send_file
+from flask import send_file
+from flask import request
+
+
