b7cfd25f2f1ac7bd076f92da66741c75f352b441
Chloe Aujoulat <chloe.aujoulat19@gmail.com>
2023-11-12 17:56:29 +0100

    add first version of the tp4

---
+++ notebooks/tp4.py
@@ -0,0 +1,18 @@
+#!usr/bin/python3
+#-*-coding: utf-8-*-
+
+import sqlite3
+from flask import Flask
+from logging import debug
+from flask import render_template
+from flask import request
+from flask import redirect
+from flask import url_for
+
+app = Flask(__name__)
+
+##################
+### TP4
+##################
+
+
